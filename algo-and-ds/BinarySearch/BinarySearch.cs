62c1d2d2011d3cdabf590abab481338d9f8f06a5
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-18 12:27:25 +0500

    Binary Search

---
+++ algo-and-ds/BinarySearch/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿namespace algo_and_ds.BinarySearch;
+
+public static class BinarySearch
+{
+    /// <summary>
+    /// Performs a binary search on a sorted array to find the specified item.
+    /// </summary>
+    /// <param name="arr">The sorted array in which to search for the item.</param>
+    /// <param name="item">The item to search for in the array.</param>
+    /// <returns>The index of the item if found; otherwise, null.</returns>
+    public static int? BinarySerach(int[] arr, int item)
+    {
+        int low = 0, high = arr.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int guess = arr[mid];
+
+            if (guess == item)
+            {
+                return mid;
+            }
+            else if (guess > item)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return null;
+    }
+}
